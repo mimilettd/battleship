49c2dfdf8497a20b43c0f3f6eea3fbe048cd1d56
mimilettd <mimilettd@gmail.com>
2017-07-15 17:01:35 -0600

    test cell changes to true when cell is selected

--- lib/game_grid.rb
+++ lib/game_grid.rb
@@ -1,18 +1,36 @@
+require 'pry'
+
 class GameGrid
   attr_reader :grid
   def initialize
-    @grid = {:a1 => false, :a2 => false, :a3 => false, :a4 => false,
-             :b1 => false, :b2 => false, :b3 => false, :b4 => false,
-             :c1 => false, :c2 => false, :c3 => false, :c4 => false,
-             :d1 => false, :d2 => false, :d3 => false, :d4 => false}
+    @grid = {"a1" => false, "a2" => false, "a3" => false, "a4" => false,
+             "b1" => false, "b2" => false, "b3" => false, "b4" => false,
+             "c1" => false, "c2" => false, "c3" => false, "c4" => false,
+             "d1" => false, "d2" => false, "d3" => false, "d4" => false}
   end
 
-  def ship_position(coord1, coord2, coord3=false)
-    # Find key
-    # Change value
-    @grid[coord1] = true
-    @grid[coord2] = true
-    @grid[coord3] = true
+  def ship_position(coord1, coord2)
+    if @grid.has_key?(coord1)
+      @grid[coord1] = true
+    end
+    convert_to_ascii(coord1, coord2)
   end
+
+  def convert_to_ascii(coord1, coord2)
+    coord1_ascii = coord1.chars.map do |n|
+      n.ord
+    end
+    coord2_ascii = coord2.chars.map do |n|
+      n.ord
+    end
+    if coord1_ascii[0] == coord2_ascii[0] && coord1_ascii[1] + 1 == coord2_ascii[1]
+      @grid[coord2] = true
+    elsif coord1_ascii[0] + 1 == coord2_ascii[0] && coord1_ascii[1] == coord2_ascii[1]
+      @grid[coord2] = true
+    else
+      puts "Ships can only be laid either horizontally or vertically!"
+    end
+  end
+
 
 end
--- test/game_grid_test.rb
+++ test/game_grid_test.rb
@@ -12,17 +12,24 @@ class GameGridTest < Minitest::Test
 
   def test_grid
     grid = GameGrid.new
-    assert_equal :a1, grid.grid.keys[0]
-    assert_equal :b1, grid.grid.keys[4]
-    assert_equal :c1, grid.grid.keys[8]
-    assert_equal :d1, grid.grid.keys[12]
+    assert_equal "a1", grid.grid.keys[0]
+    assert_equal "b1", grid.grid.keys[4]
+    assert_equal "c1", grid.grid.keys[8]
+    assert_equal "d1", grid.grid.keys[12]
   end
 
   def test_can_change_coordinates_to_occupied
     game_grid = GameGrid.new
-    game_grid.ship_position(:a1, :a2)
-    assert true, game_grid.grid[:a1]
-    assert true, game_grid.grid[:a2]
+    game_grid.ship_position("a1", "a2")
+    assert_equal true, game_grid.grid["a1"]
+    assert_equal true, game_grid.grid["a2"]
+  end
+
+  def test_false_if_coordinates_are_not_sequential
+    game_grid = GameGrid.new
+    game_grid.ship_position("a1", "c1")
+    assert_equal true, game_grid.grid["a1"]
+    assert_equal false, game_grid.grid["c1"]
   end
 
 end
