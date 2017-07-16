0cfe66cd7e734518d99dfa971551c19c6962b613
mimilettd <mimilettd@gmail.com>
2017-07-15 22:43:09 -0600

    test middle unit switch from false to true for three-unit ship given valid coordinates

--- lib/game_grid.rb
+++ lib/game_grid.rb
@@ -13,10 +13,10 @@ class GameGrid
     if @grid.has_key?(coord1)
       @grid[coord1] = true
     end
-    validating_two_unit_ship(coord1, coord2)
+    validating_two_unit_ship_position(coord1, coord2)
   end
 
-  def validating_two_unit_ship(coord1, coord2)
+  def validating_two_unit_ship_position(coord1, coord2)
     coord1_ascii = coord1.chars.map do |n|
       n.ord
     end
@@ -28,10 +28,57 @@ class GameGrid
     elsif coord1_ascii[0] + 1 == coord2_ascii[0] && coord1_ascii[1] == coord2_ascii[1]
       @grid[coord2] = true
     else
-      puts "Ships can only be laid either horizontally or vertically! Please enter the squares for the two-unit ship again."
+      puts "Incorrect ship placement. Please enter the squares for the two-unit ship again."
       @grid[coord1] = false
     end
   end
 
+  def three_unit_ship_position(coord1, coord2)
+    if @grid.has_key?(coord1)
+      @grid[coord1] = true
+    end
+    validating_three_unit_ship_position(coord1, coord2)
+  end
+
+  def validating_three_unit_ship_position(coord1, coord2)
+    coord1_ascii = coord1.chars.map do |n|
+      n.ord
+    end
+    coord2_ascii = coord2.chars.map do |n|
+      n.ord
+    end
+    if coord1_ascii[0] == coord2_ascii[0] && coord1_ascii[1] + 2 == coord2_ascii[1]
+      @grid[coord2] = true
+      convert_horizontal_middle_cell(coord2_ascii)
+    elsif coord1_ascii[0] + 2 == coord2_ascii[0] && coord1_ascii[1] == coord2_ascii[1]
+      @grid[coord2] = true
+      convert_vertical_middle_cell(coord2_ascii)
+    else
+      puts "Incorrect ship placement. Please enter the squares for the three-unit ship again."
+      @grid[coord1] = false
+    end
+  end
+
+  def convert_horizontal_middle_cell(coord2_ascii)
+    coord3_ascii = []
+    coord3_ascii << coord2_ascii[0]
+    coord3_ascii << coord2_ascii[1] - 1
+    coord3_char = coord3_ascii.map do |character|
+      character.chr
+    end
+    coord3 = coord3_char.join
+    @grid[coord3] = true
+  end
+
+  def convert_vertical_middle_cell(coord2_ascii)
+    coord3_ascii = []
+    coord3_ascii << coord2_ascii[0] - 1
+    coord3_ascii << coord2_ascii[1]
+    coord3_char = coord3_ascii.map do |character|
+      character.chr
+    end
+    coord3 = coord3_char.join
+    @grid[coord3] = true
+  end
 
 end
--- test/game_grid_test.rb
+++ test/game_grid_test.rb
@@ -32,4 +32,34 @@ class GameGridTest < Minitest::Test
     assert_equal false, game_grid.grid["c1"]
   end
 
+  def test_three_unit_ship_validation
+    game_grid = GameGrid.new
+    game_grid.three_unit_ship_position("a1", "a3")
+    assert_equal true, game_grid.grid["a1"]
+    assert_equal true, game_grid.grid["a3"]
+  end
+
+  def test_three_unit_ship_invalid_coordinates
+    game_grid = GameGrid.new
+    game_grid.three_unit_ship_position("a1", "c3")
+    assert_equal false, game_grid.grid["a1"]
+    assert_equal false, game_grid.grid["c3"]
+  end
+
+  def test_mid_cell_switch_to_true_if_three_unit_ship_laid_horizontal
+    game_grid = GameGrid.new
+    game_grid.three_unit_ship_position("a1", "a3")
+    assert_equal true, game_grid.grid["a1"]
+    assert_equal true, game_grid.grid["a2"]
+    assert_equal true, game_grid.grid["a3"]
+  end
+
+  def test_mid_cell_switch_to_true_if_three_unit_ship_laid_vertical
+    game_grid = GameGrid.new
+    game_grid.three_unit_ship_position("a1", "c1")
+    assert_equal true, game_grid.grid["a1"]
+    assert_equal true, game_grid.grid["b1"]
+    assert_equal true, game_grid.grid["c1"]
+  end
+
 end
