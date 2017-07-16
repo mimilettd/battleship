16f9f963095f5301e2abac09466d06712bec0bb2
mimilettd <mimilettd@gmail.com>
2017-07-15 21:27:32 -0600

    update game_grid method names

--- lib/game_grid.rb
+++ lib/game_grid.rb
@@ -9,14 +9,14 @@ class GameGrid
              "d1" => false, "d2" => false, "d3" => false, "d4" => false}
   end
 
-  def ship_position(coord1, coord2)
+  def two_unit_ship_position(coord1, coord2)
     if @grid.has_key?(coord1)
       @grid[coord1] = true
     end
-    convert_to_ascii(coord1, coord2)
+    validating_two_unit_ship(coord1, coord2)
   end
 
-  def convert_to_ascii(coord1, coord2)
+  def validating_two_unit_ship(coord1, coord2)
     coord1_ascii = coord1.chars.map do |n|
       n.ord
     end
@@ -28,7 +28,8 @@ class GameGrid
     elsif coord1_ascii[0] + 1 == coord2_ascii[0] && coord1_ascii[1] == coord2_ascii[1]
       @grid[coord2] = true
     else
-      puts "Ships can only be laid either horizontally or vertically!!"
+      puts "Ships can only be laid either horizontally or vertically! Please enter the squares for the two-unit ship again."
+      @grid[coord1] = false
     end
   end
 
--- test/game_grid_test.rb
+++ test/game_grid_test.rb
@@ -10,7 +10,7 @@ class GameGridTest < Minitest::Test
     assert_instance_of GameGrid, grid
   end
 
-  def test_grid
+  def test_game_grid
     grid = GameGrid.new
     assert_equal "a1", grid.grid.keys[0]
     assert_equal "b1", grid.grid.keys[4]
@@ -18,17 +18,17 @@ class GameGridTest < Minitest::Test
     assert_equal "d1", grid.grid.keys[12]
   end
 
-  def test_can_change_coordinates_to_occupied
+  def test_when_given_coordinates_false_switches_to_true
     game_grid = GameGrid.new
-    game_grid.ship_position("a1", "a2")
+    game_grid.two_unit_ship_position("a1", "a2")
     assert_equal true, game_grid.grid["a1"]
     assert_equal true, game_grid.grid["a2"]
   end
 
   def test_false_if_coordinates_are_not_sequential
     game_grid = GameGrid.new
-    game_grid.ship_position("a1", "c1")
-    assert_equal true, game_grid.grid["a1"]
+    game_grid.two_unit_ship_position("a1", "c1")
+    assert_equal false, game_grid.grid["a1"]
     assert_equal false, game_grid.grid["c1"]
   end
 
